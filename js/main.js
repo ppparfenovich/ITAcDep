1e770fe2df1bcb0064e5849406daf5910ebeedd9
Pavel Parfenovich <ppparfenovich@gmail.com>
2021-02-19 13:14:48 +0300

    remove slider

--- js/main.js
+++ js/main.js
@@ -50,32 +50,32 @@ function modalOpen() {
 
 // Slider
 
-const buttonLeft = document.querySelector('.slider__button_left');
-const buttonRight = document.querySelector('.slider__button_right');
-const slides = document.querySelectorAll('.card');
+// const buttonLeft = document.querySelector('.slider__button_left');
+// const buttonRight = document.querySelector('.slider__button_right');
+// const slides = document.querySelectorAll('.card');
 
 
 
-let currentPosition = 0;
+// let currentPosition = 0;
 
-function setPosition(position) {
-    if (position > 0) {
-        return false;
-    }
-    if (position < -(slides.length-3)*100) {
-        return false;
-    }
-    currentPosition = position;    
-    slides.forEach(slide => slide.style.transform = `translateX(${currentPosition}%)`);
-    return currentPosition;
-}
+// function setPosition(position) {
+//     if (position > 0) {
+//         return false;
+//     }
+//     if (position < -(slides.length-3)*100) {
+//         return false;
+//     }
+//     currentPosition = position;    
+//     slides.forEach(slide => slide.style.transform = `translateX(${currentPosition}%)`);
+//     return currentPosition;
+// }
 
-buttonRight.onclick = () => {
-    setPosition(currentPosition-100);
-}
+// buttonRight.onclick = () => {
+//     setPosition(currentPosition-100);
+// }
 
-buttonLeft.onclick = () => {
-    setPosition(currentPosition+100);
-}
+// buttonLeft.onclick = () => {
+//     setPosition(currentPosition+100);
+// }
 
 
