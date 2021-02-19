4609adae33605dbfad2ce052b9d86fe3dc942612
Pavel Parfenovich <ppparfenovich@gmail.com>
2021-02-19 13:03:35 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,66 @@ btnModal.forEach(function (params) {
 });
 
 function modalOpen() {
-    this.nextElementSibling.classList.toggle('modal_active');
+    this.nextElementSibling.classList.add('modal_active');
+
+    let fadeClose = document.querySelector('.modal_active');
+
+    fadeClose.onclick = function(){
+        this.classList.remove('modal_active');
+    }
+
+    let btnClose = document.querySelector('.modal_active .btn-dark');
+
 };
+
+// const fadeClose = document.querySelector('.modal_active');
+// const btnClose = document.querySelector('.modal_active .btn-dark');
+
+
+
+// fadeClose.addEventListener('click', modalClose);
+
+// function modalClose() {
+//     this.parentNode.classList.toggle('modal_active');
+// };
+
+
+ 
+// btnClose.onclick = function(){
+//     fadeClose.classList.remove('modal_active');
+// }
+
+
+
+
+// Slider
+
+const buttonLeft = document.querySelector('.slider__button_left');
+const buttonRight = document.querySelector('.slider__button_right');
+const slides = document.querySelectorAll('.card');
+
+
+
+let currentPosition = 0;
+
+function setPosition(position) {
+    if (position > 0) {
+        return false;
+    }
+    if (position < -(slides.length-3)*100) {
+        return false;
+    }
+    currentPosition = position;    
+    slides.forEach(slide => slide.style.transform = `translateX(${currentPosition}%)`);
+    return currentPosition;
+}
+
+buttonRight.onclick = () => {
+    setPosition(currentPosition-100);
+}
+
+buttonLeft.onclick = () => {
+    setPosition(currentPosition+100);
+}
+
 
